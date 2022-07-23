9c05c0390b33ec26e998a2725b464a3c4032c335
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-07-23 23:01:15 +0530

    Roll dice

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,35 @@ import React from "react";
 import Die from "./Die";
 
 export default function App(){
+
+    const[dice,setDice] = React.useState(allNewDice())
+    console.log("Set state of die",dice)
+
+    function allNewDice(){
+
+        const dice = [];
+        for(let i = 0; i < 10; i++){
+            dice.push(Math.ceil(Math.random()*6));
+        }
+        return dice;
+    }
+    // const newDie = allNewDice()
+    // console.log("Die---->",allNewDice())
+        const diceArray = dice.map(item => (
+            <Die value={item}/>
+        ))
+
+    function rollDice(){
+        setDice(allNewDice());
+    }
+
     return (
         <main>
             <div className="die-container">
-                <Die value={1}/>
-                <Die value={2}/>
-                <Die value={3}/>
-                <Die value={4}/>
-                <Die value={5}/>
-                <Die value={6}/>
-                <Die value={6}/>
-                <Die value={6}/>
-                <Die value={6}/>
-                <Die value={6}/>
+                {diceArray}
             </div>
+
+            <div onClick={rollDice} className="roll-btn"><span>Roll</span></div>
           
         </main>
     )
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,6 @@
 
 export default function Die(props){
-        console.log(props)
+        // console.log(props)
     return (
         <div className="die">
             <h2>{props.value}</h2>
